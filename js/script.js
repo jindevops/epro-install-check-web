89063dd577da11d2bab65ecfb058cfd60468b9cc
jindevops <jindev@huray.net>
2020-02-22 18:06:56 +0900

    최초 커밋 & 구현 완료 - 접속 브라우저 정보를 이용한 인스톨 버전체크 웹페이지

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+/** */
+var DEVICE_TYPE = {
+    ANDROID: 'android',
+    IPHONE: 'iphone',
+    ETC: 'etc'
+}
+
+/** 
+ * 
+ */
+var ANDROID_THRESHOLD = {
+    type: DEVICE_TYPE.ANDROID,
+    version: 5
+};
+
+var IPHONE_THRESHOLD = {
+    type: DEVICE_TYPE.IPHONE,
+    version: '10_1'
+};
+
+function getDeviceInfo() {
+    var userAgent = navigator.userAgent.toLocaleLowerCase(); 
+    var result = {
+        type: null,
+        version: null,
+        userAgent: null,
+        isAvailable: null
+    }
+    if ( userAgent.indexOf(DEVICE_TYPE.ANDROID) >= 0 ) { 
+        var androidVersion = userAgent.slice(userAgent.indexOf("android")+8).split(';')[0]; 
+        result.type = DEVICE_TYPE.ANDROID;
+        result.version = parseFloat(androidVersion);
+        result.isAvailable = checkAndroid(result);
+        result.userAgent = userAgent;
+    } else if ( userAgent.indexOf(DEVICE_TYPE.IPHONE) >= 0 ) {
+        var iPhoneVerion =  userAgent.slice(userAgent.indexOf("iphone os ")+10).split(' ')[0];
+        result.type = DEVICE_TYPE.IPHONE;
+        result.version = iPhoneVerion;
+        result.isAvailable = checkIPhone(result);
+        result.userAgent = userAgent;
+    } else {
+        result.type = DEVICE_TYPE.ETC;
+        result.version = '미분류';
+        result.useAgent = userAgent;
+        result.isAvailable = false;
+    }
+
+    return result;
+}
+
+function checkAndroid(result) {
+    var isAvailable = false;
+    var threshold = ANDROID_THRESHOLD;
+    if (threshold.type === result.type) {
+        if (result.version - threshold.version >= 0) {
+            isAvailable = true;
+        }
+    }
+    return isAvailable;
+}
+
+function checkIPhone(result) {
+    var isAvailable = false;
+    var threshold = IPHONE_THRESHOLD;
+    if (threshold.type === result.type) {
+        var resultVerSplit = result.version.split('_').map(elem => parseInt(elem));
+        var thresholdVerSplit = threshold.version.split('_').map(elem => parseInt(elem));
+        // 앞자리 큰 경우
+        if (resultVerSplit[0] - thresholdVerSplit[0] > 0) {
+            isAvailable = true;
+        }
+        // 앞자리 같은 경우
+        else if (resultVerSplit[0] === thresholdVerSplit[0] && resultVerSplit[1] - thresholdVerSplit[1] >= 0) {
+            isAvailable = true;
+        }
+    }
+    return isAvailable;
+}
